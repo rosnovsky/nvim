220ebf0345fa886a0cba544aede2b53cff7813f4
Art Rosnovsky <artem@rosnovsky.us>
2023-04-05 14:06:24 -0700

    init

---
+++ init.lua
@@ -0,0 +1,36 @@
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
+
+vim.cmd [[
+  augroup lsp_hover
+    autocmd!
+    autocmd CursorHold * silent! lua vim.lsp.buf.hover()
+    set updatetime=1000
+  augroup END
+
+  augroup Prettier
+    autocmd!
+    autocmd BufWritePre *.js,*.ts,*.jsx,*.tsx,*.json,*.css,*.scss,*.md,*.html PrettierAsync
+  augroup end
+
+  nnoremap gp :silent %!prettier --stdin-filepath %<CR>
+]]
---
+++ lua/plugins/configs/lspconfig.lua
@@ -0,0 +1,60 @@
+dofile(vim.g.base46_cache .. "lsp")
+require "nvchad_ui.lsp"
+
+local M = {}
+local utils = require "core.utils"
+
+-- export on_attach & capabilities for custom lspconfigs
+
+M.on_attach = function(client, bufnr)
+  client.server_capabilities.documentFormattingProvider = false
+  client.server_capabilities.documentRangeFormattingProvider = false
+
+  utils.load_mappings("lspconfig", { buffer = bufnr })
+
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>r r", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", {noremap = true, silent = true})
+
+  if client.server_capabilities.signatureHelpProvider then
+    require("nvchad_ui.signature").setup(client)
+  end
+end
+
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.completion.completionItem = {
+  documentationFormat = { "markdown", "plaintext" },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolveSupport = {
+    properties = {
+      "documentation",
+      "detail",
+      "additionalTextEdits",
+    },
+  },
+}
+
+require("lspconfig").tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = {
+      prefix = "■",
+      spacing = 2,
+    },
+    signs = true,
+    update_in_insert = true,
+  }
+)
+
+return M
